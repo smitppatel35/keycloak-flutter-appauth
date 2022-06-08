305885575b2410c69aa6ae310e9c231e593d5f5f
Smit Patel <smitppatel35@gmail.com>
2022-06-08 22:26:48 +0530

    .

---
+++ lib/helper/constants.dart
@@ -0,0 +1,7 @@
+const AUTH_DOMAIN = "auth.minity.tech";
+const AUTH_CLIENT_ID = "flutter-client";
+const AUTH_ISSUER = "https://auth.minity.tech/auth/realms/drilling-today";
+const BUNDLE_IDENTIFIER = "me.smitppatel35.keycloakflutter";
+const AUTH_REDIRECT_URI = "$BUNDLE_IDENTIFIER://login-callback";
+const AUTH_REFRESH_TOKEN_KEY = 'refresh-token';
+const AUTH_REALMS = "drilling-today";
